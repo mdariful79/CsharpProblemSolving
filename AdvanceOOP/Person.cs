1147dc7a8566c15d3dfd9f0493cc50bb16350caa
mdariful79 <mdariful057@gmail.com>
2026-03-09 03:04:03 +0600

    Added AdvanceOPP Project used inheritance and Const,Readonly

---
+++ AdvanceOOP/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceOOP
+{
+    public class Person
+    {
+        protected int[] grades;
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public Person(int size) // Constructor
+        {
+            grades = new int[size];
+        }
+        ~Person()
+        {
+            grades = new int[0]; // Destructor or Finalizer
+        }
+        public void SetGrade(int[] grades)
+        {
+            this.grades = grades;
+        }
+
+    }
+}
---
+++ AdvanceOOP/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceOOP
+{
+    public class Student : Person
+    {
+        private const string idPrefix = "C-";
+        private readonly int _id; 
+       
+        public Student(int id) : base(100)
+        {
+            _id = id;
+        }
+        public string GetStudentId()
+        {
+            return idPrefix + _id;
+        }
+        public double CalculateCgpa()
+        {
+            var cgpa = 0.0;
+            foreach (var grade in grades)
+            {
+                cgpa += grade;
+            }
+            return cgpa / grades.Length;
+
+        }
+    }
+}
