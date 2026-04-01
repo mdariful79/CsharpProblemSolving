0d902a52a22bd1e3db7d828049d8e3c06c73f745
ariful.islam <ariful.islam@SCI-XB-L21-1657>
2026-04-01 12:26:30 +0600

    Generic

---
+++ ExGeneric/ExBubbleSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ExGeneric
+{
+    public class ExBubbleSort<T> where T : INumber<T>
+    {
+        public T[] Sort(T[] number)
+        {
+            bool flag = true;
+            T temp;
+            int numLength = number.Length;
+
+            //sorting an array
+            for (int i = 1; (i <= (numLength - 1)) && flag; i++)
+            {
+                flag = false;
+                for (int j = 0; j < (numLength - 1); j++)
+                {
+                    if (number[j + 1] > number[j])
+                    {
+                        temp = number[j];
+                        number[j] = number[j + 1];
+                        number[j + 1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+            return number;
+        }
+
+    }
+}
---
+++ ExGeneric/Program.cs
@@ -0,0 +1,10 @@
+﻿using ExGeneric;
+
+ExBubbleSort<decimal> bubbleSort = new ExBubbleSort<decimal>();
+
+var sortedNumbers = bubbleSort.Sort(new decimal[] {3.7m ,5.1m, 2.7m, 9.3m, 1m, 5m, 6m , 3.6m});
+
+foreach(var number in sortedNumbers)
+{
+    Console.WriteLine(number);
+}
